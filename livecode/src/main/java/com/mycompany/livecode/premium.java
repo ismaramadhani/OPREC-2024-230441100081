c61c5050ceff48dbbfa41cfa8133c4eae47e5bb5
ismaramadhani <143859180+ismaramadhani@users.noreply.github.com>
2024-07-10 11:54:11 +0700

    Add files via upload

--- livecode/src/main/java/com/mycompany/livecode/premium.java
+++ livecode/src/main/java/com/mycompany/livecode/premium.java
@@ -9,5 +9,58 @@ package com.mycompany.livecode;
  * @author HP VICTUS
  */
 public class premium {
+    String tipekamar;
+    int hargakamar = 250000;
+    String namapenyewa;
+    int lama;
+    String member;
     
+    public void settipekamar(String tipekamar){
+        this.tipekamar = tipekamar;
+    }
+    
+    public String gettipekamar(){
+        return tipekamar;
+    }
+    
+    public void sethargakamar(int hargakamar){
+        this.hargakamar = hargakamar;
+    }
+    
+    public int gethargakamar(){
+        return hargakamar * lama;
+    }
+    
+    public void setnamapenyewa(String namapenyewa){
+        this.namapenyewa = namapenyewa;
+    }
+    
+    public String getnamapenyewa(){
+        return namapenyewa;
+    }
+    
+    public int diskon(){
+        if (gethargakamar() > 700000){
+            return hargakamar * 7/100;
+        } 
+        else if (gethargakamar() > 500000) {
+            return hargakamar * 5/100;
+        } else {
+            return hargakamar;
+        }  
+    }
+    public int lamasewa(){
+        if (lama > 7){
+            return hargakamar * 10/100;
+        }else {
+            return 0;
+        }
+    }
+    public int diskontambahan(){
+        if (member == "y"){
+            return hargakamar * 5/100;
+        }else{
+            return 0;
+        }
+    }
 }
