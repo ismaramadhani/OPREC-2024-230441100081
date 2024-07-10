23ab55b29112842dc4d051c32ba2e749915211b5
ismaramadhani <143859180+ismaramadhani@users.noreply.github.com>
2024-07-10 11:50:01 +0700

    Add files via upload

---
+++ livecode/src/main/java/com/mycompany/livecode/Livecode.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.livecode;
+import java.util.Scanner;
+/**
+ *
+ * @author HP VICTUS
+ */
+public class Livecode {
+
+    public static void main(String[] args) {
+        System.out.println("=============");
+        System.out.println("sewa kamar");
+        System.out.println("=============");
+        System.out.println("pilih tipe yang anda inginkan!");
+        System.out.println("1. reguler (kapasistas 1 orang)");
+        System.out.println("   harga : Rp.100.000");
+        System.out.println("2. double reguler (kapasitas 2xorang)");
+        System.out.println("   harga : Rp.200.000");
+        System.out.println("3. premium (kapasitas 2 orang)");
+        System.out.println("   harga : Rp.250.000");
+        System.out.println("4. deluxe (kapasitas 2 orang)");
+        System.out.println("   harga : Rp.350.000");
+        System.out.println("5. super premium (kapasitas 2 orang)");
+        System.out.println("   harga : Rp.500.000");
+        System.out.println("masukkan pilihan andaa : ");
+        int pilihan = inputnextInt();
+        Inputextline();
+      
+    }
+}
